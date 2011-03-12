9fa1fa2ef6f8f0d10c0950caaaae7b506e0fc534
Fabian <faebu3007@gmail.com>
2011-03-12 19:27:29 +0100

    initial sbt structure

---
+++ project/Project.scala
@@ -0,0 +1,22 @@
+import sbt._
+import java.io.File
+
+// simple build tool does not find the so libraries automatically, 
+// so we need to import them manually 
+
+class MCCProject(info: ProjectInfo) extends DefaultProject(info) {
+    // to specify new runJVMOptions we need to fork the execution    
+    override def fork = Some(new ForkScalaRun {
+        val (os, separator) = System.getProperty("os.name").split(" ")(0).toLowerCase match {
+            case "linux" => "linux" -> ":"
+            case "mac" => "macosx" -> ":"
+            case "windows" => "windows" -> ";"
+            case "sunos" => "solaris" -> ":"
+            case x => x -> ":"
+        }
+        
+        override def runJVMOptions = super.runJVMOptions ++ Seq("-Djava.library.path=" + System.getProperty("java.library.path") + separator + ("lib" / "native" / os))
+        
+        override def scalaJars = Seq(buildLibraryJar.asFile, buildCompilerJar.asFile)
+    })
+}
