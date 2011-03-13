857f5e07386eeed15160563e876ee36051e789eb
Fabian <faebu3007@gmail.com>
2011-03-13 20:11:38 +0100

    Ein Würfel anzeigen/verschieben geht

--- src/main/scala/Wuerfel.scala
+++ src/main/scala/Wuerfel.scala
@@ -23,10 +23,18 @@ class Wuerfel {
     GL11.glMatrixMode(GL11.GL_MODELVIEW);
     GL11.glTranslatef(5, 5, 0)
     GL11.glRotatef(-10, 1, 1, 1)
+    GL11.glEnable(GL11.GL_DEPTH_TEST)
+    GL11.glDepthFunc(GL11.GL_LEQUAL)
+    GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT,GL11.GL_NICEST)
 
     import Wuerfel._
-    val w = List(Face((0,0,0), 0, Left, (1,0,1)),
-                 Face((0,0,0), 0, Right, (1,0,1)))
+    /*val w = List(Face((0,0,0), 0, Left, (1,0,1)),
+                 Face((0,0,0), 0, Right, (1,0,1)),
+                 Face((0,0,0), 0, Up, (1,0,1)),
+                 Face((0,0,0), 0, Down, (1,0,1)),
+                 Face((0,0,0), 0, Front, (1,0,1)),
+                 Face((0,0,0), 0, Back, (1,0,1))) */
+    val w = Cube((0.5,0.5,0.5),2)
 
 
     while (!Display.isCloseRequested()) {
@@ -41,12 +49,12 @@ class Wuerfel {
       // draw quad
       GL11.glBegin(GL11.GL_QUADS);
 
-      for {f <- w
+      for {f <- w.faces
            p <- f.points
       } {
         val c = f.color
-        GL11.glColor3f(c._1, c._2, c._3)
-        GL11.glVertex3f(p._1, p._2, p._3)
+        GL11.glColor3d(c._1, c._2, c._3)
+        GL11.glVertex3d(p._1, p._2, p._3)
       }
       GL11.glEnd();
 
@@ -59,8 +67,9 @@ class Wuerfel {
 
 object Wuerfel {
 
-  type Point = (Float, Float, Float)
-  type Color = (Float, Float, Float)
+  // Should be some kind of a mathematical Vector
+  type Point = (Double, Double, Double)
+  type Color = (Double, Double, Double)
   sealed abstract class Orientation
   case object Left extends Orientation
   case object Right extends Orientation
@@ -69,44 +78,34 @@ object Wuerfel {
   case object Front extends Orientation
   case object Back extends Orientation
   case class Face(center: Point, size: Float, orientation: Orientation, color: Color) {
-    val points = orientation match {
-      case Left => ((0, 0, 0) ::
-                    (0, 1, 0) ::
-                    (0, 1, 1) ::
-                    (0, 0, 1) :: Nil)
-      case Right => ((1, 0, 1) ::
-                     (1, 1, 1) ::
-                     (1, 1, 0) ::
-                     (1, 0, 0) :: Nil)
+    val lfd = (-1, -1, -1)
+    val rfd = ( 1, -1, -1)
+    val lbd = (-1, -1,  1)
+    val rbd = ( 1, -1,  1)
+    val lfu = (-1,  1, -1)
+    val rfu = ( 1,  1, -1)
+    val lbu = (-1,  1,  1)
+    val rbu = ( 1,  1,  1)
+
+    val points = {
+      val cs = orientation match {
+        case Left => (lbd :: lbu :: lfu :: lfd :: Nil).reverse
+        case Right => (rfd :: rfu :: rbu :: rbd :: Nil).reverse
+        case Front => (lfu :: rfu :: rfd :: lfd :: Nil).reverse
+        case Back => (lbd :: rbd :: rbu :: lbu :: Nil).reverse
+        case Up => (lbu :: rbu :: rfu :: lfu :: Nil).reverse
+        case Down => (lfd :: rfd :: rbd :: lbd :: Nil).reverse
+      }
+      cs.map(c => (c._1 * size + center._1, c._2 * size + center._2, c._3 * size + center._3))
+      // Would be much easier with .* and .+
     }
   }
 
-  /*val w =
-      ((0, 0, 0) ::
-      (1, 0, 0) ::
-      (1, 1, 0) ::
-      (0, 1, 0) :: Nil) ::
-      ((0, 0, 0) ::
-      (0, 1, 0) ::
-      (0, 1, 1) ::
-      (0, 0, 1) :: Nil) ::
-      ((0, 0, 0) ::
-      (0, 0, 1) ::
-      (1, 0, 1) ::
-      (1, 0, 0) :: Nil) ::
-      ((1, 1, 1) ::
-      (1, 1, 0) ::
-      (0, 1, 0) ::
-      (0, 1, 1) :: Nil) ::
-      ((1, 1, 1) ::
-      (1, 1, 0) ::
-      (0, 1, 0) ::
-      (0, 1, 1) :: Nil) ::
-      ((1, 1, 1) ::
-      (1, 1, 0) ::
-      (0, 1, 0) ::
-      (0, 1, 1) :: Nil) :: Nil*/
-
+  case class Cube(center: Point, size: Float) {
+    val r = new scala.util.Random
+    val f: Orientation => Face = Face(center, size, _, (r.nextFloat, r.nextFloat, r.nextFloat))
+    val faces = (f(Left) :: f(Right) :: f(Up) :: f(Down) :: f(Front) :: f(Back) :: Nil)
+  }
 
   def main(args: Array[String]) = {
     val w = new Wuerfel
