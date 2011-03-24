65437e1ccc5673affa018c6e4577040e173e7311
Fabian <faebu3007@gmail.com>
2011-03-24 09:19:12 +0100

    Added scalatest

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,7 +4,7 @@ import java.io.File
 // simple build tool does not find the so libraries automatically, 
 // so we need to import them manually 
 
-class MCCProject(info: ProjectInfo) extends DefaultProject(info) {
+class Project(info: ProjectInfo) extends DefaultProject(info) {
   // to specify new runJVMOptions we need to fork the execution
   override def fork = Some(new ForkScalaRun {
     val (os, separator) = System.getProperty("os.name").split(" ")(0).toLowerCase match {
@@ -19,6 +19,7 @@ class MCCProject(info: ProjectInfo) extends DefaultProject(info) {
     override def runJVMOptions = super.runJVMOptions ++ Seq("-Djava.library.path=" + System.getProperty("java.library.path") + separator + asdf)
     override def scalaJars = Seq(buildLibraryJar.asFile, buildCompilerJar.asFile)
   })
+
   val lwjglRepo = "lwjglRepo" at "http://adterrasperaspera.com/lwjgl"
   val lwjgl = "org.lwjgl" % "lwjgl" % "2.7.1"
   val lwjglUtil = "org.lwjgl" % "lwjgl-util" % "2.7.1"
@@ -26,6 +27,8 @@ class MCCProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val slickRepo = "slickRepo" at "http://slick.cokeandcode.com/mavenrepo/"
   val slick = "slick" % "slick" % "262"
+
+  val scalaTest = "org.scalatest" % "scalatest" % "1.3"
     
 }
 
